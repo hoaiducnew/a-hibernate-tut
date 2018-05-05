6013aecaeaf67303d8e4f0e3cb4bd5b8d8f7bee1
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-05-05 11:41:49 -0500

    - Implementing deleteById JPA Repository Method

--- hibernate-tut-01/src/main/java/com/example/hibernatetut01/HibernateTut01Application.java
+++ hibernate-tut-01/src/main/java/com/example/hibernatetut01/HibernateTut01Application.java
@@ -28,9 +28,7 @@ public class HibernateTut01Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 //		logger.info("All users -> {}", dao.findAll());
 		logger.info("User id 10001 -> {}", repository.findById(10001));
-//		logger.info("Deleting 10002 -> No of Rows Deleted - {}", dao.deleteById(10002));
-//		logger.info("Inserting 10004 -> {}", dao.insert(new Person(10004, "Tara", "Berlin", new Date())));
-//		logger.info("Updating 10003 -> {}", dao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
+		repository.deleteById(10002);
 		logger.info("Inserting 10004 -> {}", repository.insert(new Person("Tara", "Berlin", new Date())));
 		logger.info("Updating 10003 -> {}", repository.update(new Person(10003, "Pieter", "Utrecht", new Date())));
 	}
