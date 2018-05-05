35d81623d138843fdd056048e00fd286b3496134
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-05-05 10:56:39 -0500

    - Implement findAll persons Spring JDBC Query Method - Execute the findAll method using CommandLineRunner

--- hibernate-tut-01/src/main/java/com/example/hibernatetut01/HibernateTut01Application.java
+++ hibernate-tut-01/src/main/java/com/example/hibernatetut01/HibernateTut01Application.java
@@ -1,12 +1,28 @@
 package com.example.hibernatetut01;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.hibernatetut01.jdbc.PersonJbdcDao;
+
 @SpringBootApplication
-public class HibernateTut01Application {
+public class HibernateTut01Application implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJbdcDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateTut01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All users -> {}", dao.findAll());
 	}
 }
---
+++ hibernate-tut-01/src/main/java/com/example/hibernatetut01/jdbc/PersonJbdcDao.java
@@ -0,0 +1,21 @@
+package com.example.hibernatetut01.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.hibernatetut01.entity.Person;
+
+@Repository
+public class PersonJbdcDao {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	public List<Person> findAll() {
+		return jdbcTemplate.query("select * from person", 
+				new BeanPropertyRowMapper(Person.class));
+	}
+}
