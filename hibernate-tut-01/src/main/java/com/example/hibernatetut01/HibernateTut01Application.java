44ae7cbd26eb07e3e3b326464a1eb5158fe9852a
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-05-05 11:06:32 -0500

    - Implementing insert and update Spring JDBC Update Methods

--- hibernate-tut-01/src/main/java/com/example/hibernatetut01/HibernateTut01Application.java
+++ hibernate-tut-01/src/main/java/com/example/hibernatetut01/HibernateTut01Application.java
@@ -1,5 +1,7 @@
 package com.example.hibernatetut01;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.hibernatetut01.entity.Person;
 import com.example.hibernatetut01.jdbc.PersonJbdcDao;
 
 @SpringBootApplication
@@ -26,5 +29,7 @@ public class HibernateTut01Application implements CommandLineRunner {
 		logger.info("All users -> {}", dao.findAll());
 		logger.info("User id 10001 -> {}", dao.findById(10001));
 		logger.info("Deleting 10002 -> No of Rows Deleted - {}", dao.deleteById(10002));
+		logger.info("Inserting 10004 -> {}", dao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+		logger.info("Updating 10003 -> {}", dao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
 	}
 }
