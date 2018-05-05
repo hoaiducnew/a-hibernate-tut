806f8cb3a56c3b76c6c28f43281b05d2c666d177
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-05-05 10:39:39 -0500

    init proj hibernate-tut-01

---
+++ hibernate-tut-01/src/main/java/com/example/hibernatetut01/HibernateTut01Application.java
@@ -0,0 +1,12 @@
+package com.example.hibernatetut01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTut01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateTut01Application.class, args);
+	}
+}
