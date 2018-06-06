92e77eabef909e1ec90eab9fdf69c749fbdabdc6
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-06-06 16:08:39 -0400

    Lazy Fetch

---
+++ hibernate-tut-01/src/test/java/com/example/hibernatetut01/jpa/StudentRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.hibernatetut01.jpa;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.hibernatetut01.HibernateTut01Application;
+import com.example.hibernatetut01.entity.Student;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HibernateTut01Application.class)
+public class StudentRepositoryTest {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	StudentRepository repository;
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	@Transactional // important
+	public void retrieveStudentAndPassportDetails() {
+		Student student = em.find(Student.class, 20001L);
+		logger.info("student -> {}", student);
+		logger.info("passport -> {}", student.getPassport());
+	}
+}
